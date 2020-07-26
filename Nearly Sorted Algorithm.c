5fd55d6f00fdf3b229c2cd2ead3617c80a813b18
HIMANSHU SHARMA <ranubhardwaj89@gmail.com>
2020-07-26 21:20:05 +0530

    Create Nearly Sorted Algorithm.c

---
+++ Nearly Sorted Algorithm.c
@@ -0,0 +1,33 @@
+// Given an array of n elements, where each element is at most k away from its target position. The task is to print array in sorted form.
+
+#include<stdio.h>
+void sort(int a[],int n,int k)
+{
+    int key,j;
+    for(int i=1;i<n;i++)
+    {
+        key=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>key)
+        {
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=key;
+    }
+}
+int main()
+{
+	int num;scanf("%d",&num);
+	while(num--)
+	{
+	    int n,k;
+	    scanf("%d%d",&n,&k);int a[n];
+	    for(int i=0;i<n;i++)
+	    scanf("%d",&a[i]);
+	    sort(a,n,k);
+	    for(int i=0;i<n;i++)
+	    printf("%d ",a[i]);
+	    printf("\n");
+	}
+}
